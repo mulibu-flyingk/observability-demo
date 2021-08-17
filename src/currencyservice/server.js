46608283a34a78f200fba62c2c2f1fdd32d78878
Christoph Dittmann <mulibu.flyingk@gmail.com>
2021-08-17 10:31:48 +0200

    remove stackdriver tracing, profiler and debugging and added otel tracing

--- src/currencyservice/server.js
+++ src/currencyservice/server.js
@@ -14,39 +14,12 @@
  * limitations under the License.
  */
 
-if(process.env.DISABLE_PROFILER) {
-  console.log("Profiler disabled.")
-}
-else {
-  console.log("Profiler enabled.")
-  require('@google-cloud/profiler').start({
-    serviceContext: {
-      service: 'currencyservice',
-      version: '1.0.0'
-    }
-  });
-}
-
-
 if(process.env.DISABLE_TRACING) {
   console.log("Tracing disabled.")
 }
 else {
   console.log("Tracing enabled.")
-  require('@google-cloud/trace-agent').start();
-}
-
-if(process.env.DISABLE_DEBUGGER) {
-  console.log("Debugger disabled.")
-}
-else {
-  console.log("Debugger enabled.")
-  require('@google-cloud/debug-agent').start({
-    serviceContext: {
-      service: 'currencyservice',
-      version: 'VERSION'
-    }
-  });
+  require('./tracing.js');
 }
 
 const path = require('path');
